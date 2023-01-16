29945829fd2ed07a372f42a17355e247783c3000
orviz <orviz@ifca.unican.es>
2023-01-16 13:42:59 +0100

    Add JePL file structure

---
+++ .sqa/script.slaphappy-bronze-mole.sh
@@ -0,0 +1,4 @@
+(
+cd git.man.poznan.pl/stash/scm/eosc-rs/nearest-neighbor-finder &&
+    hadolint */Dockerfile* --failure-threshold error
+)
---
+++ .sqa/script.trippy-lilac-mouse.sh
@@ -0,0 +1,4 @@
+(
+cd git.man.poznan.pl/stash/scm/eosc-rs/nearest-neighbor-finder &&
+    bandit -f json --quiet  --confidence-level high --severity-level high --recursive .
+)
